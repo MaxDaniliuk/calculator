4d11fc7c541ed40a8c7f6c37396120fd48eefc3c
Maksim Daniliuk <maksim.daniliuk@gmail.com>
2023-12-29 00:10:20 +0200

    Evaluate and show one pair of numbers at a time

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
-//function add(a, b) {return a + b}
-//function subtract(a, b) {return a - b}
-//function multiply(a, b) {return a * b}
-//function divide(a, b) {return b == 0 ? "Error" : a / b}
 function findPercentage(a) {return a / 100}
 function changeSign(a) {return a * (-1)}
-
-//Focus on adding 3 + 5 and showing 8
 
 function startCalculation() {
     let operatorHolder = '';
     let number = '';
     let containerArr = [];
     const displayCurrentValue = document.querySelector('.current-text');
+    const displayOperation = document.querySelector('.previous-text');
     const numberBtns = document.querySelectorAll('.number');
     const operationBtns = document.querySelectorAll('.operation');
-    //5 + 3
+    const clearAllBtn = document.querySelector('#clear-all');
+    let calculator = new Calculator;
+
     numberBtns.forEach(numberBtn => {
         numberBtn.addEventListener('click', () => {
             number += numberBtn.textContent;
             displayCurrentValue.textContent = number;
-            //containerArr.push(numberBtn.textContent);
-            
         });
     });
 
@@ -31,29 +26,36 @@ function startCalculation() {
                 containerArr.push(number);
                 number = '';
             }
+            if (containerArr.length == 3) {
+                let calculationResult = String(calculator.calculate(containerArr));
+                console.log(calculationResult);
+                containerArr = [calculationResult, operatorHolder]
+            }
             if (!isNaN(+containerArr[containerArr.length - 1])) {
                 containerArr.push(operationBtn.textContent);
             } else if (containerArr[containerArr.length - 1] == '+' || containerArr[containerArr.length - 1] == '-' || containerArr[containerArr.length - 1] == 'x' || containerArr[containerArr.length - 1] == '/') {
                 containerArr[containerArr.length - 1] = operationBtn.textContent
-            } 
+            }
+            displayOperation.textContent = containerArr.join(' ');
+            displayCurrentValue.textContent = containerArr[0];
             console.log(containerArr);
         });
     });
-    /*
-    function controlArrayLength(array, operationSign) {
-    if (isNaN(+array[array.length - 1])) {
-        return array[array.length - 1] = operationSign
-    }
-}
-    */
+
+    clearAllBtn.addEventListener('click', () => {
+        number = '';
+        displayCurrentValue.textContent = 0;
+        displayOperation.textContent = '';
+        containerArr = [];
+    });
 }
 
 function Calculator() {
     this.methods = {
         "+": (a, b) => a + b,
         "-": (a, b) => a - b,
-        "*": (a, b) => a * b,
-        "/": (a, b) => b == 0 ? "Error" : a / b,
+        "x": (a, b) => a * b,
+        "/": (a, b) => b == 0 ? "Error" : (a / b).toFixed(2),
     }
 
     this.calculate = function(array) {
@@ -69,7 +71,6 @@ function Calculator() {
         }
     }
 }
-
 
 
 
